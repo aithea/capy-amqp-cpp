a5df73636fbbb8643770f76600b10a783f3eef05
Денис Свинарчук <denn.nevera@gmail.com>
2019-05-31 16:47:56 +0300

    initial api refs

--- include/capy_amqp.h
+++ include/capy_amqp.h
@@ -4,9 +4,65 @@
 
 #pragma once
 
+#include <optional>
+#include <string>
+#include <vector>
+#include <functional>
+#include <system_error>
+
+//#include "json.hpp"
+
 namespace capy::amqp {
 
-    class Connection {
+    using namespace std;
+
+    typedef string json;
+
+    typedef std::function<void(const std::error_code &code)> ErrorHandler;
+
+    typedef std::function<void(const json &message)> MessageHandler;
+
+    class Url {
+    };
+
+    /**
+     *
+     */
+    class Exchange {
+
+    public:
+
+        /**
+         *
+         * @param url
+         * @param exchange_name
+         * @return
+         */
+        static optional<Exchange> Bind(const Url &url, const string &exchange_name="amq.topic");
+
+        /**
+         *
+         * @param message
+         * @param keys
+         */
+        void fetch(const json& message, const vector<string>& keys);
+
+        /***
+         *
+         * @param message
+         * @param keys
+         */
+        void publish(const json& message, const string& queue_name);
+
+        /**
+         *
+         * @param queue_name
+         * @param on_data
+         */
+        void listen(const string& queue_name, const MessageHandler& on_data);
+
+    protected:
+        Exchange(const Url &url, const string &exchange_name);
 
     };
 
--- src/capy_amqp.cpp
+++ src/capy_amqp.cpp
@@ -4,3 +4,29 @@
 
 #include "capy_amqp.h"
 
+namespace capy::amqp {
+
+    optional<Exchange> Exchange::Bind(const capy::amqp::Url &url, const std::string &exchange_name) {
+      if (true){
+        return make_optional(Exchange(url,exchange_name));
+      }
+      return nullopt;
+    }
+
+    Exchange::Exchange(const capy::amqp::Url &url, const std::string &exchange_name) {
+
+    }
+
+    void Exchange::fetch(const capy::amqp::json &message, const std::vector<std::string> &keys) {
+
+    }
+
+    void Exchange::listen(const std::string &queue_name, const capy::amqp::MessageHandler &on_data) {
+      on_data(json());
+    }
+
+    void Exchange::publish(const capy::amqp::json &message, const std::string &queue_name) {
+
+    }
+
+}
